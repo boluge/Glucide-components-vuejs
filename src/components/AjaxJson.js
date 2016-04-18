d0120ba76e1d160dbf29a156312f946965a6e1a0
Stephane <boluge@gmail.com>
2016-04-18 17:00:15 +0200

    up

--- src/components/AjaxJson.js
+++ src/components/AjaxJson.js
@@ -1,20 +1,22 @@
 export default class ajaxJson {
   constructor (url) {
     this.url = url
-    if (window.XMLHttpRequest) {
-      xhr = new XMLHttpRequest()     //  Firefox, Safari, ...
-    }
-    else  if (window.ActiveXObject) {
-      xhr = new ActiveXObject("Microsoft.XMLHTTP")
-    }
+    // var request
+    var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
+    // if (window.XMLHttpRequest) {
+    var request = new XMLHttpRequest()
+    // } else {
+    //  request = new ActiveXObject('Microsoft.XMLHTTP')
+    // }
+    request.setRequestHeader('Host', 'xxx')
     request.open('GET', this.url, true)
     request.send()
     request.onreadystatechange = function () {
       console.log(this.readyState)
       if (this.readyState === 4) {
         return this.responseText
-        // console.log(this.responseText)
       }
     }
   }
 }
+// https://vuejs.org/examples/commits.html
